4e8f56ca730f760a4cb46c724cb2388850c74171
daigo <dragonmind2002@yahoo.co.jp>
2012-02-01 08:27:06 +0900

    【公開実装中】rails環境作り直し

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shohari::Application.config.secret_token = '4c76ce5122a6fce26b4de6cbce9633ec63038b626f24d9d83003d7869b3f221857021e330a0423dcd472a9d2a1d3542bbc53d1f2beebd553b7ae7d90c7f6762a'
